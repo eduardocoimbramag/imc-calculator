a0cfddd44d86e3d33ad4cad42f423d7db93f7125
eduardocoimbramag <eduardocoimbramag@gmail.com>
2024-02-07 22:59:19 -0300

    adding imc formula js file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,12 +18,33 @@ function run() {
             height: height.value
         }
 
-        pessoas.push();
-        pessoas.push(personInfo);
-        console.log(pessoas);
+        pessoas.push(personInfo)
+
+        const imc = personInfo.weight / personInfo.height ** 2
+
+        console.log(imc)
+
+        let msg;
+
+        if (imc > 0 && imc <= 18.5) {
+            msg = `Your IMC is ${imc.toFixed(2)}. You are underweight.`
+        } else if (imc > 18.5 && imc <= 24.9) {
+            msg = `Your IMC is ${imc.toFixed(2)}. You are at the ideal weight, congratulations!`
+        } else if (imc > 24.9 && imc <= 29.9) {
+            msg = `Your IMC is ${imc.toFixed(2)}. You are slightly overweight.`
+        } else if (imc > 29.9 && imc <= 34.9) {
+            msg = `Your IMC is ${imc.toFixed(2)}. You have obesity grade 1.`
+        } else if (imc > 34.9 && imc <= 39.9) {
+            msg = `Your IMC is ${imc.toFixed(2)}. You have obesity grade 2.`
+        } else if (imc > 39.9) {
+            msg = `Your IMC is ${imc.toFixed(2)}. You have obesity grade 3.`
+        }
+
+
+        console.log(msg)
 
         const print = document.getElementById("result")
-        print.innerHTML = `Name: ${personInfo.name}, Age: ${personInfo.age}, Weight: ${personInfo.weight}, Height: ${personInfo.height}`;
+        print.innerHTML = `Hello, ${personInfo.name}. ${msg}`;
     };
     form.addEventListener("submit", addEvent);
 }
